91816dec1c36ac49f965958801b93acb064799ea
Paula <paulhaconde@gmail.com>
2023-02-07 12:55:46 -0500

    Solucionar error de ver interfaz en el navegador

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import logo from '../assets/LogoWC.png';
+//import './Login.css';
 
-function Login (){
+const Login = () => {
     const navigate = useNavigate();
+    const responseGoogle = (response) => {
+    };
+
+
+    return (
+        <div className="login">
+            <section className="login-form"/>
+            <img className="logo" alt="logo" src={logo}/>
+            <h3>Lorem ipsum dolor sit amet. Aut amet molestiae est enim perferendis qui
+                dicta magnam eos quia distinctio! Sed eveniet quasi id inventore iure ea distinctio sapiente.
+            </h3>
+        </div>
+    );
+
+
+
+
 }
 
 
@@ -11,4 +30,5 @@ function Login (){
 
 
 
-export default Login;
+
+   export default Login;
